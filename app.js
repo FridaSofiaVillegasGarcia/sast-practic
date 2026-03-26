abb306b61124520d6085a398bf7016ccfcb622e0
FRIDA SOFIA <112402240+FridaSofiaVillegasGarcia@users.noreply.github.com>
2026-03-26 01:53:03 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-// Ejemplo con vulnerabilidades
-
 // ❌ Hardcoded secret
 const password = "123456";
 
-// ❌ Uso inseguro de eval
+// ❌ Uso peligroso
 function ejecutar(codigo) {
     eval(codigo);
 }
 
-// ❌ Simulación de SQL Injection
+// ❌ SQL Injection
 function getUser(id) {
     const query = "SELECT * FROM users WHERE id = " + id;
     console.log(query);
